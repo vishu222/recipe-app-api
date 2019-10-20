28836632ff9bc16c40e0001e99d67153a63575e3
vishu222 <vishwajeet.manish@galepartners.com>
2019-10-20 20:23:00 +0530

    added endpoint for tags

---
+++ app/recipe/tests/test_tag_api.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagsAPITest(TestCase):
+    """test the publicly available tags api"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """test that login is required for retrieving tags"""
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagApiTest(TestCase):
+    """test the authorized user tags api"""
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@vishwa.com', 'pass123'
+            )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieving_tags(self):
+        """test retrieving tags"""
+        Tag.objects.create(user=self.user, name='vegan')
+        Tag.objects.create(user=self.user, name='dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """test that tags returned are only for the authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'user@user2.com', 'testpass2'
+            )
+        Tag.objects.create(user=user2, name='fruity')
+        tag = Tag.objects.create(user=self.user, name='comfort food')
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
---
+++ app/recipe/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """retrieve the tags only belonging to the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
