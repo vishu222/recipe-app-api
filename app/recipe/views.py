5e207d8b7b3bd66e5bdb7af6a10e56a1372d215b
vishu222 <vishwajeet.manish@galepartners.com>
2019-11-23 18:46:51 +0530

    added fitler feature to the recipe api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,9 +43,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, )
     queryset = Recipe.objects.all()
 
+    def _param_to_int(self, qs):
+        """Convert a list of string ids to list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """retireve recipes only assigned to the authenticated user"""
-        return self.queryset.filter(user=self.request.user)
+
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._param_to_int(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._param_to_int(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
